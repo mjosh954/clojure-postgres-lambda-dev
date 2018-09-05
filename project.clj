192ccf7d6729566fa249f44c80a63d1ff245e333
Josh <mjosh954@gmail.com>
2018-09-05 01:19:07 -0500

    added ring server

--- project.clj
+++ project.clj
@@ -5,5 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
     [org.clojure/clojure "1.8.0"]
+    [ring/ring-core "1.6.3"]
+    [ring/ring-jetty-adapter "1.6.3"]
+    [ring/ring-json "0.4.0"]
     [korma "0.4.0"]
-    [org.postgresql/postgresql "42.2.5.jre7"]])
+    [org.postgresql/postgresql "42.2.5.jre7"]]
+  :main ^:skip-aot command.core)
--- src/command/core.clj
+++ src/command/core.clj
@@ -1,13 +1,32 @@
 (ns command.core
   (:gen-class
-    :methods [^:static [handler [String] String]]
-    :refer [command.dbent]))
+    :methods [^:static [handler [String] String]])
+    (:use ring.middleware.params
+      ring.util.response
+      ring.adapter.jetty))
+(require '[command.dbent :as db])
 
 
 (defn find-command
   [command]
   (str "Searching for " command " command"))
 
-(defn -handler 
-  [s]
-  (str "Hello " s "!"))
+; (defn -handler 
+;   [s]
+;   (str "Hello " s "!"))
+
+(defn build-response 
+  [status-code body]
+  {:status status-code
+    :headers {"Content-Type" "text/plain"}
+    :body body})
+
+(defn handler [{{command "command"} :params}]
+  (if (nil? command)
+    (build-response 404 "Provide a command query param. (ex ?command=doc)")
+    (build-response 200 (db/query-command-by-name command))))
+
+(def app
+  (-> handler wrap-params))
+
+(run-jetty app {:port 8080})
